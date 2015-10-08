0676fceda8b200115aaacd7c95026197966a293e
David Vicklund <david@vicklund.com>
2015-10-07 20:30:37 -0700

    many changes: add app folder and lib folder with modularized functions in js files

--- server.js
+++ server.js
@@ -1,36 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var port = process.env.PORT || 3000;
-
-
-function Adjective() {
-  this.ecstatic = true;
-  this.euphoric = true;
-  this.enormous = true;
-  this.evil = true;
-  this.enumerated = true;
-  this.egregious = true;
-  this.elaborate = true;
-  this.energetic = true;
-  this.eclairish = true;
-}
-
+var Adjective = require("./lib/adjective.js");
+var getRandomWord = require("./lib/getRandomWord.js");
+var Verb = require("./lib/verb.js");
+var Noun = require("./lib/noun.js");
 var adjective = new Adjective();
 
-function getRandomWord(object) {
-  var propArray = Object.keys(object);
-  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
-  return {word: randomProp};
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/app/'));
+
+var verb = new Verb();
 
 getRandomWord(adjective);
 
-function Nouns() {
-  this.elephant = true;
-  this.eclair = true;
-  this.egret = true;
-  this.endive = true;
-}
+var noun = new Noun();
 
 app.listen(port, function() {
   console.log("server started on port " + port);
@@ -42,4 +31,12 @@ app.get("/", function(req, res) {
 
 app.get("/adjective", function(req, res) {
   res.json(getRandomWord(adjective));
+});
+
+app.get("/noun", function(req, res) {
+  res.json(getRandomWord(noun));
+});
+
+app.get("/verb", function(req, res) {
+  res.json(getRandomWord(verb));
 });
